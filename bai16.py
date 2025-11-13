7cc7691887f36b2d0e89c0ba82e51ab095d36a3e
BlackOne1311 <136611519+BlackOneBO@users.noreply.github.com>
2025-11-13 20:27:17 +0700

    Delete bai16.py
    Signed-off-by: BlackOne1311 <136611519+BlackOneBO@users.noreply.github.com>

--- bai16.py
+++
@@ -1,6 +0,0 @@
-from math import *
-h = float(input("Nhap chieu cao vat tha roi xuong: "))
-g = 9.81
-Vi = float(input("Nhap van toc vat tha : "))
-v = sqrt(Vi**2 + 2*g*h)
-print("Van toc vat khi cham dat la:", v, "m/s")
