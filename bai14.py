5af463b8743963a05af8de47584df894a4b2ae1d
BlackOne1311 <136611519+BlackOneBO@users.noreply.github.com>
2025-11-13 20:27:09 +0700

    Delete bai14.py
    Signed-off-by: BlackOne1311 <136611519+BlackOneBO@users.noreply.github.com>

--- bai14.py
+++
@@ -1,9 +0,0 @@
-a = int(print("Nhap so nguyen a: "))
-b = int(print("Nhap so nguyen b: "))
-tong = a + b
-print("Tong hai so nguyen la:", tong)
-print("Tich hai so nguyen la:", a*b)
-print("Hieu hai so nguyen la:", a-b)
-print("Thuong hai so nguyen la:", a/b)
-print("Phan du hai so nguyen la:", a%b)
-print("a mu b la:", a**b)
